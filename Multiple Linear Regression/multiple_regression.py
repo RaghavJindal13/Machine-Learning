2e11b88d8a693a8588aa8121b43b39c0639b1b1d
RaghavJindal13 <53964007+RaghavJindal13@users.noreply.github.com>
2020-02-13 20:07:42 +0530

    Create multiple_regression.py

---
+++ Multiple Linear Regression/multiple_regression.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('housing.data', delim_whitespace=True, header=None)
+
+col_name = ['CRIM', 'ZN' , 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.columns = col_name
+df.head()
+
+X = df.iloc[:, :-1].values
+y = df.iloc[:, 13].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
